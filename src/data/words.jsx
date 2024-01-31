dd89c33e84c777c61dc5904657ef18956675d201
Pedro Henrique de Faria <99847050+PedroFaria14@users.noreply.github.com>
2024-01-31 16:42:24 -0300

    Add files via upload

---
+++ src/data/words.jsx
@@ -0,0 +1,10 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Farol", "Vidro", "Volante"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacaxi", "Uva", "Morango"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Orelha", "Dedo", "Barriga"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Memória", "Placa de Vídeo"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Node.js", "Python", "CSS"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Vegetais", "Frango", "Peixe"],
+    esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Ciclismo", "Vôlei", "Golfe"],
+    país: ["Brasil", "EUA", "França", "Japão", "Austrália", "Índia", "Canadá"],
+  };
